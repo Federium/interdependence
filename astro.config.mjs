465e516fa9953e77babc2cc252ce5f00a7af22e2
Filippo Mirandola <filippo.mirandola@outlook.com>
2025-03-27 12:43:26 +0100

    test mappa (in altri file)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     prefetchAll: true
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
-});
 
+  integrations: [react()],
+});
