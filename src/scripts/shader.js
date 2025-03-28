c046fbdc2f6dbebf2e085ae1cb793d4cb79adced
Filippo Mirandola <filippo.mirandola@outlook.com>
2025-03-28 22:10:31 +0100

    shader NON animato

--- src/scripts/shader.js
+++ src/scripts/shader.js
@@ -16,42 +16,72 @@ const vert = `
  `;
  
  function frag(number) {
-   return `precision mediump float;
+   return `// Author: @patriciogv
+// Title: CellularNoise
+
+#ifdef GL_ES
+precision mediump float;
+#endif
 
 varying vec2 vTexCoord;
+
+uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
+
+vec2 random2( vec2 p ) {
+    return fract(sin(vec2(dot(p,vec2(127.1,311.7)),dot(p,vec2(269.5,183.3))))*43758.5453);
+}
 
 void main() {
     vec2 st = vTexCoord;
 
     vec3 color = vec3(.0);
 
-    // Cell positions
-    vec2 point[5];
-    point[0] = vec2(0.83,0.75);
-    point[1] = vec2(0.60,0.07);
-    point[2] = vec2(0.28,0.64);
-    point[3] =  vec2(0.31,0.26);
-    point[4] = u_mouse;
+    // Scale
+    st *= 2.5;
+
+    // Tile the space
+    vec2 i_st = floor(st);
+    vec2 f_st = fract(st);
 
     float m_dist = 1.;  // minimum distance
 
-    // Iterate through the points positions
-    for (int i = 0; i < 5; i++) {
-        float dist = distance(st, point[i]);
+    for (int y= -1; y <= 1; y++) {
+        for (int x= -1; x <= 1; x++) {
+            // Neighbor place in the grid
+            vec2 neighbor = vec2(float(x),float(y));
 
-        // Keep the closer distance
-        m_dist = min(m_dist, dist);
+            // Random position from current + neighbor place in the grid
+            vec2 point = random2(i_st + neighbor);
+
+			// Animate the point
+            point = 0.5 + 0.25*sin(u_time + 6.2831*point);
+
+			// Vector between the pixel and the point
+            vec2 diff = neighbor + point - f_st;
+
+            // Distance to the point
+            float dist = length(diff);
+
+            // Keep the closer distance
+            m_dist = min(m_dist, dist);
+        }
     }
 
     // Draw the min distance (distance field)
     color += m_dist;
 
-    // Show isolines
-    // color -= step(.7,abs(sin(50.0*m_dist)))*.3;
+    // Draw cell center
+    // scolor += 1.-step(.02, m_dist);
 
-    gl_FragColor = vec4(color, 1.0);
+    // Draw grid
+    // color.r += step(.98, f_st.x) + step(.98, f_st.y);
+
+    // Show isolines
+    // color -= step(.7,abs(sin(27.0*m_dist)))*.5;
+
+    gl_FragColor = vec4(color,1.0);
 }
      `;
  }
