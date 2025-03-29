28f83b22f3e1cf010d251e07f6077364c9a61692
Filippo Mirandola <filippo.mirandola@outlook.com>
2025-03-29 12:08:10 +0100

    Update shader.js

--- src/scripts/shader.js
+++ src/scripts/shader.js
@@ -38,29 +38,41 @@ void main() {
     vec3 color = vec3(.0);
 
     // Cell positions
-    vec3 point[15];
+    // vec3 point[15];
+    // point[0] = vec3(0.83, 0.75, 0.50);   // Max z-value at 0.5
+    // point[1] = vec3(0.60, 0.07, 0.40);
+    // point[2] = vec3(0.28, 0.64, 0.30);
+    // point[3] = vec3(0.31, 0.26, 0.20);
+    // point[4] = vec3(0.15, 0.85, 0.10);
+    // point[5] = vec3(0.76, 0.32, 0.00);   // Middle point at 0.0
+    // point[6] = vec3(0.45, 0.50, -0.10);
+    // point[7] = vec3(0.22, 0.12, -0.20);
+    // point[8] = vec3(0.90, 0.42, -0.30);
+    // point[9] = vec3(0.55, 0.81, -0.40);
+    // point[10] = vec3(0.38, 0.92, -0.50); // Min z-value at -0.5
+    // point[11] = vec3(0.67, 0.18, 0.35);
+    // point[12] = vec3(0.12, 0.35, -0.35);
+    // point[13] = vec3(0.48, 0.67, 0.25);
+    // point[14] = u_mouse;
+
+        vec3 point[10];
     point[0] = vec3(0.83, 0.75, 0.50);   // Max z-value at 0.5
     point[1] = vec3(0.60, 0.07, 0.40);
-    point[2] = vec3(0.28, 0.64, 0.30);
-    point[3] = vec3(0.31, 0.26, 0.20);
-    point[4] = vec3(0.15, 0.85, 0.10);
-    point[5] = vec3(0.76, 0.32, 0.00);   // Middle point at 0.0
-    point[6] = vec3(0.45, 0.50, -0.10);
-    point[7] = vec3(0.22, 0.12, -0.20);
-    point[8] = vec3(0.90, 0.42, -0.30);
-    point[9] = vec3(0.55, 0.81, -0.40);
-    point[10] = vec3(0.38, 0.92, -0.50); // Min z-value at -0.5
-    point[11] = vec3(0.67, 0.18, 0.35);
-    point[12] = vec3(0.12, 0.35, -0.35);
-    point[13] = vec3(0.48, 0.67, 0.25);
-    point[14] = u_mouse;
+    point[2] = vec3(0.15, 0.85, 0.10);
+    point[3] = vec3(0.76, 0.32, 0.00);   // Middle point at 0.0
+    point[4] = vec3(0.45, 0.50, -0.10);
+    point[5] = vec3(0.55, 0.81, -0.40);
+    point[6] = vec3(0.38, 0.92, -0.50); // Min z-value at -0.5
+    point[7] = vec3(0.12, 0.35, -0.35);
+    point[8] = vec3(0.48, 0.67, 0.25);
+    point[9] = u_mouse;
 
     float max_dist = SIZE;
 
     float m_dist = max_dist; 
 
     // Iterate through the points positions
-    for (int i = 0; i < 15; i++) {
+    for (int i = 0; i < 10; i++) {
       vec3 obs = vec3(normalizedTexCoord.x, normalizedTexCoord.y, u_z);
       
       float dist = distance(obs, point[i]);
